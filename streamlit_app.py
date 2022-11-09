16eb0b3b2bb7663284d94c9fa3a74aad608d2314
shaik aakheel <117806053+SHAIK-AAKHEEL@users.noreply.github.com>
2022-11-09 13:53:06 +0530

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,11 @@
 import streamlit
 streamlit.title("My Mom's New Healthy Dinner")
 
-streamlit.header('Breakfast Favourites')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text(' Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.text(' Avocodo Toast')               
+streamlit.header('🍩 Breakfast Favourites')
+streamlit.text('🍿 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🍶 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🍖 Hard-Boiled Free-Range Egg')
+streamlit.text('🍛🍹 Avocodo Toast')               
 
 
 
