36c0f8733660f0c7d65d57211fefd0420ffc85f3
shaik aakheel <117806053+SHAIK-AAKHEEL@users.noreply.github.com>
2022-11-09 13:36:26 +0530

    Update streamlit_app.py
    typo error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Break fast menu')
+streamlit.text('omega 3 & blueberry oat meal')
+streamlit.text('kale,spinash&rocket smothier')
+streamlit.text('hard boiled free-range-egg')
+
