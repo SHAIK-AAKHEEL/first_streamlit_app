6a1d280d007d46718aaa556f24240e1fcbd546ce
shaik aakheel <117806053+SHAIK-AAKHEEL@users.noreply.github.com>
2022-11-10 18:37:09 +0530

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,6 +65,14 @@ streamlit.dataframe(my_data_rows)
 
 
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+import requests
+
+
+
+
+
 
 
 
