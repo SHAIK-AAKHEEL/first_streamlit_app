d7c72941a7819c39dccc81536eccb160b01a9182
shaik aakheel <117806053+SHAIK-AAKHEEL@users.noreply.github.com>
2022-11-10 18:57:15 +0530

    Update streamlit_app.py
    code copied in local file and changed

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
 streamlit.title("My Mom's New Healthy Dinner")
 
 streamlit.header('🍩 Breakfast Favourites')
@@ -12,7 +17,7 @@ streamlit.text('🍛🍹 Avocodo Toast')
 streamlit.header('🍌🥭Build Your Own Fruit Smoothie 🥝🍇')
 
 
-import pandas
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -29,7 +34,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-import requests
+
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 # streamlit.text(fruityvice_response.json()) # just write the data to screen
@@ -42,23 +47,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
-
-
-
-
-
-# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-# my_cur = my_cnx.cursor()
-# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-# my_data_row = my_cur.fetchone()
-# streamlit.text("Hello from Snowflake:")
-# streamlit.text(my_data_row)
-
-
-
-
-
+streamlit.stop();
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
