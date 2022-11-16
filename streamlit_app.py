47cdc4decfd88d7bc0a71be486c95a55d0cf45a3
shaik aakheel <117806053+SHAIK-AAKHEEL@users.noreply.github.com>
2022-11-16 12:08:55 +0530

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,71 @@
+
 import streamlit
-import snowflake.connector
 import pandas
-streamlit.title('Zena\'s Amazing Athleisure Catalog')
-# connect to snowflake
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-# run a snowflake query and put it all in a var called my_catalog
-my_cur.execute("select color_or_style from catalog_for_website")
-my_catalog = my_cur.fetchall()
-# put the dafta into a dataframe
-df = pandas.DataFrame(my_catalog)
-# temp write the dataframe to the page so I Can see what I am working with
-# streamlit.write(df)
-# put the first column into a list
-color_list = df[0].values.tolist()
-# print(color_list)
-# Let's put a pick list here so they can pick the color
-option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
-# We'll build the image caption now, since we can
-product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
-# use the option selected to go back and get all the info from the database
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
+streamlit.title("My Mom's New Healthy Dinner")
+streamlit.header('🍩 Breakfast Favourites')
+streamlit.text('🍿 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🍶 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🍖 Hard-Boiled Free-Range Egg')
+streamlit.text('🍛🍹 Avocodo Toast')               
+streamlit.header('🍌🥭Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+# Display the table on the page.
+streamlit.dataframe(fruits_to_show)
 
 
+# create the repetable code block (called a function)
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+#new section to display fruitvice api response
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please enter a fruit to get information.")
+  else:
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+    
+except URLError as e:
+  streamlit.error()
+        
+streamlit.header("View Our Fruit List:")
+#snowflake-related functions
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+       my_cur.execute("SELECT * from fruit_load_list")
+       return my_cur.fetchall()
+# Add button to load the fruit
+if streamlit.button('Get Fruit List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  my_cnx.close()
+  streamlit.dataframe(my_data_rows)
 
+  
+# Allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values ('"+new_fruit+"')")
+    return "Thanks for adding"+ new_fruit
+ 
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
+  
 
 
